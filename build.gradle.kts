78458c784e3e604900c46766d81d1c864a587486
Rodrigo Vianna <rodrigo.vianna.oliveira@gmail.com>
2023-04-08 16:49:08 -0300

    Issue 1 - initial structure

---
+++ build.gradle.kts
@@ -0,0 +1,102 @@
+import Dependencies.modules
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Dependencies.Gradle.kotlinPlugin)
+        classpath(Dependencies.Gradle.androidTools)
+        classpath(kotlin(Dependencies.Gradle.gradlePlugin, Versions.kotlin_version))
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    configureAndroid()
+}
+
+
+fun Project.configureAndroid() {
+    val isAppModule = name == "app"
+
+    when {
+        isAppModule -> configureAppAndroid()
+        modules.contains(name) -> configureAndroidLibrary()
+        else -> return
+    }
+
+    apply(plugin = "kotlin-android")
+    apply(plugin = "kotlin-kapt")
+
+    configure<com.android.build.gradle.BaseExtension> {
+        compileSdkVersion(Config.compileSdkVersion)
+        buildToolsVersion(Config.buildToolsVersion)
+        namespace = Config.applicationId
+
+        defaultConfig {
+            minSdk = Config.minSdkVersion
+            targetSdk = Config.targetSdkVersion
+            versionCode = Config.versionCode
+            versionName = Config.versionName
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+            vectorDrawables.useSupportLibrary = true
+            multiDexEnabled = true
+        }
+
+        lintOptions {
+            isCheckReleaseBuilds = false
+            isCheckDependencies = true
+            isCheckAllWarnings = true
+            isWarningsAsErrors = true
+            isIgnoreWarnings = true
+            isAbortOnError = false
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+}
+
+
+fun Project.configureAppAndroid() {
+    apply(plugin = "com.android.application")
+
+    configure<com.android.build.gradle.BaseExtension> {
+        defaultConfig {
+            applicationId = Config.applicationId
+        }
+
+        testOptions {
+            animationsDisabled = true
+        }
+
+        buildTypes {
+            getByName("release") {
+                isMinifyEnabled = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+    }
+}
+
+fun Project.configureAndroidLibrary() {
+    apply(plugin = "com.android.library")
+    apply(plugin = "org.jetbrains.kotlin.android")
+
+    configure<com.android.build.gradle.BaseExtension> {
+    }
+}
+
+tasks.register("clean").configure {
+    delete("build")
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,131 @@
+import kotlin.reflect.full.memberProperties
+
+interface GroupLibs {
+    val list: List<String>
+}
+
+object Dependencies {
+    const val runnerPackage = "androidx.test.runner.AndroidJUnitRunner"
+
+    object Module {
+    }
+
+    val modules: List<String> by lazy {
+        Module::class.memberProperties.map {
+            it.name
+        }
+    }
+
+    object Gradle {
+        const val kotlinPlugin =
+            "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin_version}"
+        const val androidTools = "com.android.tools.build:gradle:${Versions.gradle_plugin_version}"
+        const val gradlePlugin = "gradle-plugin"
+    }
+
+    object Kotlin : GroupLibs {
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin_version}"
+        const val reflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin_version}"
+        const val coreKtx = "androidx.core:core-ktx:${Versions.androidx_core_ktx}"
+
+        override val list: List<String>
+            get() = listOf(
+                Kotlin.stdlib,
+                Kotlin.reflect,
+                Kotlin.coreKtx
+            )
+    }
+
+    object UnitTest : GroupLibs {
+        const val core = "androidx.test:core:${Versions.test_core_version}"
+        const val junit = "junit:junit:${Versions.test_junit_version}"
+        const val runner = "androidx.test:runner:${Versions.test_core_version}"
+        const val viewModelTest = "androidx.arch.core:core-testing:${Versions.viewmodel_version}"
+        const val mockk = "io.mockk:mockk:${Versions.mockk}"
+
+        override val list: List<String>
+            get() = listOf(
+                UnitTest.core,
+                UnitTest.junit,
+                UnitTest.viewModelTest,
+                UnitTest.mockk
+            )
+    }
+
+    object AndroidTest : GroupLibs {
+        const val runner = "androidx.test:runner:${Versions.test_core_version}"
+        const val espresso =
+            "androidx.test.espresso:espresso-core:${Versions.test_espresso_version}"
+        const val espressoContrib =
+            "com.android.support.test.espresso:espresso-contrib:${Versions.test_espresso_version}"
+        const val rules = "androidx.test:rules:${Versions.test_rules_version}"
+        const val hamcrest = "org.hamcrest:hamcrest-library:${Versions.test_hamcrest}"
+        const val uiautomator = "androidx.test.uiautomator:uiautomator:${Versions.test_uiautomator}"
+        const val ext = "androidx.test.ext:junit:${Versions.test_ext}"
+        const val mockWebServer = "com.squareup.okhttp3:mockwebserver:${Versions.mockWebServer}"
+        const val intent =
+            "androidx.test.espresso:espresso-intents:${Versions.test_espresso_version}"
+
+        override val list: List<String>
+            get() = listOf(
+                AndroidTest.espresso,
+                AndroidTest.hamcrest,
+                AndroidTest.rules,
+                AndroidTest.uiautomator,
+                AndroidTest.runner,
+                AndroidTest.espressoContrib,
+                AndroidTest.mockWebServer,
+                AndroidTest.ext,
+                AndroidTest.intent
+            )
+    }
+
+    object Support : GroupLibs {
+        const val appCompat = "androidx.appcompat:appcompat:${Versions.support_version}"
+        const val androidXCore = "androidx.core:core-ktx:${Versions.support_version}"
+        const val constraintLayout =
+            "androidx.constraintlayout:constraintlayout:${Versions.constraint_motion_version}"
+        const val material = "com.google.android.material:material:${Versions.material_version}"
+        const val animation = "androidx.dynamicanimation:dynamicanimation:1.0.0"
+
+        override val list: List<String>
+            get() = listOf(
+                Support.appCompat,
+                Support.constraintLayout,
+                Support.material,
+                Support.animation
+            )
+    }
+
+    object Retrofit : GroupLibs {
+        const val core = "com.squareup.retrofit2:retrofit:${Versions.retrofit_version}"
+        const val moshi = "com.squareup.moshi:moshi-kotlin:${Versions.moshi_version}"
+        const val moshiConverter =
+            "com.squareup.retrofit2:converter-moshi:${Versions.retrofit_version}"
+        const val coreOkhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp_version}"
+        const val interceptor =
+            "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp_version}"
+
+        override val list: List<String>
+            get() = listOf(
+                Retrofit.core,
+                Retrofit.moshi,
+                Retrofit.moshiConverter,
+                Retrofit.coreOkhttp,
+                Retrofit.interceptor
+            )
+    }
+
+    object ViewModel : GroupLibs {
+        const val lifecycleExtensions =
+            "androidx.lifecycle:lifecycle-extensions:${Versions.viewmodel_version}"
+        const val lifecycleConvertRxToLivedata =
+            "androidx.lifecycle:lifecycle-reactivestreams:${Versions.viewmodel_version}"
+
+        override val list: List<String>
+            get() = listOf(
+                ViewModel.lifecycleExtensions,
+                ViewModel.lifecycleConvertRxToLivedata
+            )
+    }
+}
