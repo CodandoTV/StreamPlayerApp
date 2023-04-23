5f9a911166fec650c2cb1fbab3e17494ea7cb2bc
Rodrigo Vianna <rodrigo.vianna.oliveira@gmail.com>
2023-04-23 12:28:23 -0300

    #5 - configurando inicialmente  com a API

--- feature-list-streams/src/main/java/com/codandotv/streamplayerapp/feature_list_streams/presentation/ListMovieViewModel.kt
+++ feature-list-streams/src/main/java/com/codandotv/streamplayerapp/feature_list_streams/presentation/ListMovieViewModel.kt
@@ -2,6 +2,7 @@ package com.codandotv.streamplayerapp.feature_list_streams.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.codandotv.streamplayerapp.feature_list_streams.domain.ListStreamAnalytics
 import com.codandotv.streamplayerapp.feature_list_streams.domain.ListStreamUseCase
 import com.codandotv.streamplayerapp.feature_list_streams.domain.model.ListStream
 import kotlinx.coroutines.launch
@@ -13,7 +14,7 @@ sealed class ListStreamState {
 class ListMovieViewModel(
         private val uiModel: ListStreamUimodel,
         private val useCase: ListStreamUseCase,
-        private val analytics: com.codandotv.streamplayerapp.feature_list_streams.domain.ListStreamAnalytics
+        private val analytics: ListStreamAnalytics
 ) : ViewModel() {
     fun curtaVideo() {
         viewModelScope.launch {
