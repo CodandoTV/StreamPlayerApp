105d25b2d4f7715dd84d372ab6406cec39494ac5
carlosvaccari <carlosvaccari@gmail.com>
2023-05-06 18:26:41 -0300

    [Issue-16] - Removing unnecessary change

--- core-navigation/src/main/java/com/codandotv/streamplayerapp/core_navigation/bottomnavigation/StreamPlayerBottomNavigation.kt
+++ core-navigation/src/main/java/com/codandotv/streamplayerapp/core_navigation/bottomnavigation/StreamPlayerBottomNavigation.kt
@@ -1,12 +1,6 @@
 package com.codandotv.streamplayerapp.core_navigation.bottomnavigation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalAbsoluteTonalElevation
 import androidx.compose.material3.MaterialTheme
@@ -16,48 +10,37 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.currentBackStackEntryAsState
+import com.codandotv.streamplayerapp.core_navigation.helper.currentRoute
+
+private val bottomMenuItems = listOf(
+    BottomNavItem.Home, BottomNavItem.Games, BottomNavItem.News, BottomNavItem.Downloads
+)
 
 @Composable
 fun StreamPlayerBottomNavigation(navController: NavController) {
-    val items = listOf(
-        BottomNavItem.Home,
-        BottomNavItem.Games,
-        BottomNavItem.News,
-        BottomNavItem.Downloads
-    )
-
     NavigationBar(
         containerColor = MaterialTheme.colorScheme.surface,
         contentColor = MaterialTheme.colorScheme.onBackground,
         modifier = Modifier.height(72.dp)
     ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-        items.forEach { item ->
-            NavigationBarItem(
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = MaterialTheme.colorScheme.onBackground,
-                    unselectedIconColor = MaterialTheme.colorScheme.surfaceVariant,
-                    selectedTextColor = MaterialTheme.colorScheme.onBackground,
-                    unselectedTextColor = MaterialTheme.colorScheme.surfaceVariant,
-                    indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
-                        LocalAbsoluteTonalElevation.current
-                    )
-                ),
-                icon = {
-                    Icon(
-                        painterResource(id = if (currentRoute == item.screenRoute) item.iconSelected else item.iconUnselected),
-                        contentDescription = stringResource(id = item.title),
-                    )
-                },
+        val currentRoute = currentRoute(navController = navController)
+
+        bottomMenuItems.forEach { item ->
+            NavigationBarItem(colors = NavigationBarItemDefaults.colors(
+                selectedIconColor = MaterialTheme.colorScheme.onBackground,
+                unselectedIconColor = MaterialTheme.colorScheme.surfaceVariant,
+                selectedTextColor = MaterialTheme.colorScheme.onBackground,
+                unselectedTextColor = MaterialTheme.colorScheme.surfaceVariant,
+                indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
+                    LocalAbsoluteTonalElevation.current
+                )
+            ),
+                icon = { NavItemIcon(currentRoute, item) },
                 label = {
                     Text(
                         text = stringResource(id = item.title),
@@ -65,18 +48,32 @@ fun StreamPlayerBottomNavigation(navController: NavController) {
                     )
                 },
                 selected = currentRoute == item.screenRoute,
-                onClick = {
-                    navController.navigate(item.screenRoute) {
-                        navController.graph.startDestinationRoute?.let { screen_route ->
-                            popUpTo(screen_route) {
-                                saveState = true
-                            }
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
-            )
+                onClick = { onItemClicked(navController, item) })
         }
+    }
+}
+
+@Composable
+private fun NavItemIcon(
+    currentRoute: String?,
+    item: BottomNavItem
+) {
+    Icon(
+        painterResource(id = if (currentRoute == item.screenRoute) item.iconSelected else item.iconUnselected),
+        contentDescription = stringResource(id = item.title),
+    )
+}
+
+private fun onItemClicked(
+    navController: NavController, item: BottomNavItem
+) {
+    navController.navigate(item.screenRoute) {
+        navController.graph.startDestinationRoute?.let { screen_route ->
+            popUpTo(screen_route) {
+                saveState = true
+            }
+        }
+        launchSingleTop = true
+        restoreState = true
     }
 }
--- core-navigation/src/main/java/com/codandotv/streamplayerapp/core_navigation/helper/NavigationHelper.kt
+++ core-navigation/src/main/java/com/codandotv/streamplayerapp/core_navigation/helper/NavigationHelper.kt
@@ -2,11 +2,11 @@ package com.codandotv.streamplayerapp.core_navigation.helper
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
-fun currentRoute(navController: NavHostController): String? {
+fun currentRoute(navController: NavController): String? {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     return navBackStackEntry?.destination?.route
 }
