2851bb385a51ff716c3c2eda3c997ef491f6ce5b
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2023-05-07 11:34:51 -0300

    [ISSUE-25] - Removing unnecessary activity

--- feature-list-streams/src/main/java/com/codandotv/streamplayerapp/feature_list_streams/di/ListStreamModule.kt
+++ feature-list-streams/src/main/java/com/codandotv/streamplayerapp/feature_list_streams/di/ListStreamModule.kt
@@ -7,7 +7,7 @@ import com.codandotv.streamplayerapp.feature_list_streams.domain.ListStreamAnaly
 import com.codandotv.streamplayerapp.feature_list_streams.domain.ListStreamAnalyticsImpl
 import com.codandotv.streamplayerapp.feature_list_streams.domain.ListStreamUseCase
 import com.codandotv.streamplayerapp.feature_list_streams.domain.ListStreamUseCaseImpl
-import com.codandotv.streamplayerapp.feature_list_streams.presentation.ListStreamViewModel
+import com.codandotv.streamplayerapp.feature_list_streams.presentation.screens.ListStreamViewModel
 import com.codandotv.streamplayerapp.feature_list_streams.presentation.ListStreamUiModelImpl
 import com.codandotv.streamplayerapp.feature_list_streams.presentation.ListStreamUimodel
 import org.koin.androidx.viewmodel.dsl.viewModel
