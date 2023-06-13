88c89d2cc1b57266da7fe0d6e193fb8a948345aa
gentildpinto <gentil.junior.gp@gmail.com>
2023-06-13 16:31:48 +0100

    currentRoute helper retorna uma string vazia ou invés de null

--- core-navigation/src/main/java/com/codandotv/streamplayerapp/core_navigation/helper/NavigationHelper.kt
+++ core-navigation/src/main/java/com/codandotv/streamplayerapp/core_navigation/helper/NavigationHelper.kt
@@ -6,7 +6,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
-fun currentRoute(navController: NavController): String? {
+fun currentRoute(navController: NavController): String {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
-    return navBackStackEntry?.destination?.route
+    return navBackStackEntry?.destination?.route ?: ""
 }
