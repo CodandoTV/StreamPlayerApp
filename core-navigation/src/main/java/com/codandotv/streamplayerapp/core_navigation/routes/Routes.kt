28a5cd83c8d7c961da0ed2cfa40e0d047b4347b5
gentildpinto <gentil.junior.gp@gmail.com>
2023-06-13 16:22:12 +0100

    apaga o arquivo BottomNavRoutes.kt e coloca as rotas no arquivo Routes.kt

--- core-navigation/src/main/java/com/codandotv/streamplayerapp/core_navigation/routes/Routes.kt
+++ core-navigation/src/main/java/com/codandotv/streamplayerapp/core_navigation/routes/Routes.kt
@@ -2,10 +2,26 @@ package com.codandotv.streamplayerapp.core_navigation.routes
 
 import com.codandotv.streamplayerapp.core_navigation.routes.Routes.PARAM.ID
 
-object Routes {
-    const val DETAIL = "DetailList/"
-    const val DETAIL_COMPLETE = "${DETAIL}{${ID}}"
-    const val Splash = "splash"
+enum class Routes(
+    val route: String,
+    val hasBottomBar: Boolean = true,
+) {
+    Detail(route = "detailsList/", hasBottomBar = false),
+    DetailComplete(route = "${Detail.route}{${ID}}", hasBottomBar = false),
+    Splash(route = "splash", hasBottomBar = false),
+    Home(route = "home"),
+    Games(route = "games"),
+    News(route = "news"),
+    Scenes(route = "scenes"),
+    Downloads(route = "downloads");
+
+    companion object {
+        fun hasBottomBar(route: String): Boolean {
+            return Routes.values().find {
+                it.route == route && it.hasBottomBar
+            } != null
+        }
+    }
 
     object PARAM {
         const val ID = "id"
