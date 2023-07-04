2ce1b8ba8a673865eec9a7a743f2b57a8a5e24d7
Rodrigo Vianna <rodrigo.vianna.oliveira@gmail.com>
2023-07-04 12:29:50 -0300

    [ISSUE-57] - 🔥 colocando catalog no projeto

--- build-logic/src/main/java/Config.kt
+++ build-logic/src/main/java/Config.kt
@@ -13,7 +13,10 @@ object Config {
 
         private const val tmdb_token_name_debug = "TMDB_BEARER_TOKEN_DEBUG"
         private const val tmdb_token_name_release = "TMDB_BEARER_TOKEN_RELEASE"
-        val api_bearer_debug = "\"Bearer ${System.getenv(tmdb_token_name_debug) ?: ""}\""
-        val api_bearer_release = "\"Bearer ${System.getenv(tmdb_token_name_release) ?: ""}\""
+
+        private const val bearear_without_environment = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNDg2NWM4YTAzNzhmM2I4NjI0OWU1ZjNiYWFiMjU2NyIsInN1YiI6IjY0Mjk4YTg5YTNlNGJhMWM0NDgzM2U4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9cIxv29vkaZ2yW88DIFRUFK_nXbK2b6KS8t96kA8WAE"
+
+        val api_bearer_debug = "\"Bearer ${System.getenv(tmdb_token_name_debug) ?: bearear_without_environment}\""
+        val api_bearer_release = "\"Bearer ${System.getenv(tmdb_token_name_release) ?: bearear_without_environment}\""
     }
 }
